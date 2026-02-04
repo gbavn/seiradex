8954911e4c0951d6beb803c615db9b1f582706e6
gbavn <kkamg10@gmail.com>
2026-02-04 16:53:18 -0300

    Remove 'R03' from locked maps list

--- utils/hide-config.js
+++ utils/hide-config.js
@@ -50,7 +50,7 @@ window.SEIRA_HIDDEN = {
     // NÃO aparecem em buscas, cards ou "onde encontrar"
     locked: {
         maps: [
-            'R02', 'R03', 'R08', 'R09', 'R10', 'R11', 'AZR', 'MTL', 'ARD', 'SLV', 'CRG', 'HBZ', 'SAN', 'MTGL', 'PTVY', 'DSRT', 'SELV', 'VULC', 'ILHA', 'FLSB', 'BSQP'
+            'R02', 'R08', 'R09', 'R10', 'R11', 'AZR', 'MTL', 'SLV', 'CRG', 'HBZ', 'SAN', 'MTGL', 'PTVY', 'DSRT', 'SELV', 'VULC', 'ILHA', 'FLSB', 'BSQP'
         ]
     },
     
